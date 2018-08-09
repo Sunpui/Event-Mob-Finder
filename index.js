890cb7a4ec668ea69b7805d198ccb9410e50dc8f
Bubble <33262142+Sunpui@users.noreply.github.com>
2018-08-09 10:28:50 +0200

    BigInt fix for pinkie's proxy
    Added BigInt compatibility for pinkies proxy, should now work for both proxies

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Command = require('command');
 module.exports = function EventMobFinder(dispatch) {
 	const command = Command(dispatch);
 	let enabled = true;
-	npcs = [];
+	let npcs = [];
 	
 	command.add('eventmob', () => {
 		enabled = !enabled;
@@ -12,29 +12,37 @@ module.exports = function EventMobFinder(dispatch) {
 	});
 	
 	dispatch.hook('S_SPAWN_NPC', 8, event => {
-		if(event.unk20<<24>>24 === 1) {
+		if((event.unk20&0xFF) === 1) {
+			npcs[Number(event.gameId)] = event;
+			var source;
+			if (dispatch.base.proxyAuthor == "caali") {
+				event.gameId = {low:event.gameId.low, high:0, unsigned:true};
+				source = 0;
+			} else {
+				event.gameId = event.gameId & BigInt(0xFFFF);
+				source = BigInt(0);
+			}
 			dispatch.toClient('S_SPAWN_DROPITEM', 6, {
-				gameId: {low:event.gameId.low,high:0,unsigned:true},
+				gameId: event.gameId,
 				loc: event.loc,
 				item: 98260,
 				amount: 1,
-				expiry: 300000,
-				explode:false,
-				masterwork:false,
-				enchant:0,
-				source:0,
-				debug:false,
+				expiry: 30000,
+				explode: false,
+				masterwork: false,
+				enchant: 0,
+				source: source,
+				debug: false,
 				owners: [{id: 0}]
 			});
-			npcs[event.gameId] = event;
 			notice();
 			command.message('Found event mob.');
 		}
 	});
 	
 	dispatch.hook('S_DESPAWN_NPC', 3, event => {
-		if(npcs[event.gameId]) {
-			dispatch.toClient('S_DESPAWN_DROPITEM', 1, {id:{low:event.gameId.low,high:0,unsigned:true}} );
+		if(npcs[Number(event.gameId)]) {
+			dispatch.toClient('S_DESPAWN_DROPITEM', 1, {id:npcs[event.gameId].gameId} );
 			delete npcs[event.gameId];
 		}
 	});
